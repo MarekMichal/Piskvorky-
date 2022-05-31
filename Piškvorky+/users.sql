6a6406de819bcc92d66f9c56e5fad2c83974f818
TomasFojtik <73063267+TomasFojtik@users.noreply.github.com>
2022-05-31 18:09:22 +0200

    Add files via upload

--- Piškvorky+/users.sql
+++ Piškvorky+/users.sql
@@ -16,11 +16,11 @@ CREATE TABLE  `users` (
   `nick` varchar(50) COLLATE utf8_slovak_ci NOT NULL,
   `email` varchar(50) COLLATE utf8_slovak_ci NOT NULL,
   `password` varchar(50) COLLATE utf8_slovak_ci NOT NULL,
-  `money` int(11) NOT NULL,
+  /*`money` int(11) NOT NULL,
   `vyhry` int(11) NOT NULL,
   `prehry` int(11) NOT NULL,
   `remizy` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`)*/
 ) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_slovak_ci;
 
 
